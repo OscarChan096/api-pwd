fbcc67b293a3970c745729bc29f018e78af8b890
oscar <oscarchan96@outlook.com>
2022-11-05 00:06:39 -0600

    fix: cors error

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
 import pwdRoutes from './routes/pwd.routes';
+import cors from 'cors';
 
 const app = express();
 app.set('port', 5000);
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false})); // para recibir formularios html
-app.use('/api/pwd',pwdRoutes);
+app.use(express.urlencoded({ extended: false })); // para recibir formularios html
+app.use('/api/pwd', pwdRoutes);
+app.use(cors({origin: '*'}));
 
 export default app;
